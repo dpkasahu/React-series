6354eb9bcb0daadacba8281a7c2524439d19fa85
Deepika <deepikasahu2053@gmail.com>
2025-01-29 20:19:51 +0530

    React

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Deepika Sahu</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Deepika Sahu | vitereactseries</h1>
+    </div>
+  )
+}
+
+export default App
